48a508d8b1256a34eb43f5fd4d17051c43951cc8
alex <alexisoro99@gmail.com>
2022-12-21 23:17:53 -0800

    adding code files

---
+++ apbp.js
@@ -0,0 +1,49 @@
+const arr = [
+    {name: 'Elie'}, 
+    {name: 'Tim'}, 
+    {name: 'Matt'}, 
+    {name: 'Colt'}]
+
+// extractValue:
+function extractValue(arr, key) {
+     return arr.reduce(function(acc, next){
+        acc.push(next[key]);
+        return acc;
+    },[]);
+}
+
+// vowelCount: Found this one the most confusing
+function vowelCount(str){
+    const vowels = "aeiou";
+    return str.split('').reduce(function(acc,next){
+        let lowerCased = next.toLowerCase()
+        if(vowels.indexOf(lowerCased) !== -1){
+            if(acc[lowerCased]){
+                acc[lowerCased]++;
+            } else {
+                acc[lowerCased] = 1;
+            }
+        }
+        return acc;
+    }, {});
+}
+
+// addKeyAndValue:
+function addKeyAndValue(arr, key, value){
+    return arr.reduce(function(acc,next,idx){
+        acc[idx][key] = value;
+        return acc;
+    },arr);
+}
+
+// partition:
+function partition(arr, cb){
+    return arr.reduce(function(acc,next){
+        if(cb(next)){
+            acc[0].push(next);
+        } else {
+            acc[1].push(next);
+        }
+        return acc;
+    }, [[],[]]);
+}
